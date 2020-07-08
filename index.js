e0ce1d62f210875843c68295418bd74f2ab167e1
NightcoreZena <66201378+NightcoreZena@users.noreply.github.com>
2020-07-08 09:21:12 +0200

    Update

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ const YouTube = require('simple-youtube-api')
 const { util } = require('simple-youtube-api')
 const PREFIX = 'n!'
 
-///////////////////////////////////////////////////////
-const client = new Client({ disableEveryone: true })//
-const youtube = new YouTube(apikey)//////////////////
-const queue = new Map()/////////////////////////////
-///////////////////////////////////////////////////
+
+const client = new Client({ disableEveryone: true })
+const youtube = new YouTube(apikey)
+const queue = new Map()
 
 let readys = ['print ("Hello World")', 'Hello World.']
 let randomready = readys[Math.floor(Math.random() * readys.length)]
 
-client.on('ready', () => console.log(randomready))
+client.on('ready', async () => {
+console.log(randomready)
+let statuses = [` | ${PREFIX}help.`, ` | Music in ${client.guilds.cache.size} guilds.`, ` | ${client.user.username}!`]
+
+  setInterval(function () {
+    let status = statuses[Math.floor(Math.random() * statuses.length)]
+
+    client.user.setActivity(status, { type: "listening" })
+  }, 2000)
+})
+
+
+
+
 
 
 
